f8798ef1c2c9dc5550a20794298047d3367db13f
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2018-08-14 11:36:12 -0400

    Changed URL for paper; Updates vignettes via devtools::build_vignettes()

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -3,39 +3,6 @@ knitr::opts_chunk$set(echo = TRUE)
 library(knitr)
 set.seed(12314159)
 
-## ----directory, echo = FALSE, eval = TRUE--------------------------------
-imageDirectory <- "./img"
-path_concat <- function(path1, path2, sep="/") {paste(path1, path2, sep = sep)}
-
-## ----figure-helper, eval = TRUE, echo = FALSE----------------------------
-### Helper functions
-is.substring <- function (substring, string) {
-    grepl(substring, string)
-}
-
-devOn <- function (forPrint, width, height, filename, units = "in", filetype=c("png", "pdf")){
-    if (forPrint) {
-        filetype <- match.arg(filetype)
-        switch(filetype,
-               "png" = {
-                   if (!is.substring("png", filename)) {filename <- paste(filename,"png",sep=".")}
-                   png(filename, width=width, height=height, units=units, res=100, bg="transparent")
-                   
-               },
-               "pdf" = {
-                   if (!is.substring("pdf", filename)) {filename <- paste(filename,"pdf",sep=".")}
-                   pdf(filename, width=width, height=height, units = units, bg="transparent")
-               },
-               stop("Unrecognized filetype")
-        )
-    }
-}
-
-
-devOff <- function (forPrint){
-    if (forPrint) {dev.off()}
-}
-
 ## ----libraries-----------------------------------------------------------
 library(eikosograms)
 library(gridExtra)
